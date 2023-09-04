20a8fbf18b8203d4b84c270332d77d77d7baa9af
112hyer <143990481+112hyer@users.noreply.github.com>
2023-09-05 05:16:53 +0900

    project01

---
+++ src/subject2/script/reference.js
@@ -0,0 +1,9 @@
+const reference = [
+  {
+    author: "KBO(한국야구위원회)",
+    title: "야구 기록법 소개",
+    year: "",
+    sourceTitle: "",
+    link: "https://www.koreabaseball.com/About/Committee/RecordRaw.aspx",
+  },
+;
